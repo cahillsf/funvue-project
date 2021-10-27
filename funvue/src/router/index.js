06972336ed1f580f9d7b49a57f2bbdf722de2516
cahillsf <stephen.cahill@datadoghq.com>
2021-10-27 16:08:00 -0400

    adding routing for createaccount page

--- funvue/src/router/index.js
+++ funvue/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import AnotherPage from '@/components/AnotherPage'
+import CreateAccount from '@/components/CreateAccount'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/nextpage',
       name: 'AnotherPage',
       component: AnotherPage
+    },
+    {
+      path: '/createaccount',
+      name: 'CreateAccount',
+      component: CreateAccount
     }
   ]
 })
