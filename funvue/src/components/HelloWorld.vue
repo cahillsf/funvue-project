edf38486c4e9e88970bf65dda44cb4c32dbe4163
Steve Cahill <cahillsf9@gmail.com>
2021-11-23 14:09:10 -0500

    modifying headline text

--- funvue/src/components/HelloWorld.vue
+++ funvue/src/components/HelloWorld.vue
@@ -36,7 +36,7 @@ export default {
   },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App',
+      msg: 'Our Flagship Products',
       cardProps: {
         'grid-column': 0,
         'grid-row': 0
