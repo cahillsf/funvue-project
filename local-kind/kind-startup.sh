b81199d9e82f4af55ec84b62f93ab202be5d97d7
cahillsf <stephen.cahill@datadoghq.com>
2023-07-27 17:49:29 -0400

    add load testing to docker compose and local-kind

--- local-kind/kind-startup.sh
+++ local-kind/kind-startup.sh
@@ -4,5 +4,7 @@ kind create cluster --config kind-cluster-final.yaml
 kind export kubeconfig --name fv-kind
 DATADOG_API_SECRET_NAME=datadog-api-secret
 kubectl create secret generic $DATADOG_API_SECRET_NAME --from-literal api-key=$DD_API_KEY
-helm install ddagent -f values.yaml  --set datadog.apiKeyExistingSecret=datadog-api-secret datadog/datadog
+kubectl create configmap my-loadtest-locustfile --from-file ../locust-loadtest/locustfile.py
+helm install ddagent -f dd-values.yaml  --set datadog.apiKeyExistingSecret=datadog-api-secret datadog/datadog
+helm install locust deliveryhero/locust -f locust-values.yaml
 kubectl apply -f ../k8s-config -R
--- locust-loadtest/locustfile.py
+++ locust-loadtest/locustfile.py
@@ -12,11 +12,5 @@ class QuickstartUser(HttpUser):
     def get_cards(self):
         self.client.get("/api/cards")
 
-    # @task(3)
-    # def view_items(self):
-    #     for item_id in range(10):
-    #         self.client.get(f"/item?id={item_id}", name="/item")
-    #         time.sleep(1)
-
     def on_start(self):
         self.client.post("/api/userAuth", auth=('test@gmail.com', 'testing'))
